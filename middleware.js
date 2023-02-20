a2432ae44137f07b2c25344fe6eb6fc80909d590
Guillem Andreu <git@guillemandreu.com>
2023-02-20 21:51:49 +0100

    build: upgrade dependencies

--- middleware.js
+++ middleware.js
@@ -4,18 +4,18 @@ export function middleware (req) {
   const { headers, nextUrl } = req
 
   if (nextUrl.pathname === '/' || nextUrl.pathname.startsWith('/api')) {
-    const basicAuth = headers.get('authorization')
+    const basicAuth = headers.get('Authorization')
 
     if (basicAuth) {
       const auth = basicAuth.split(' ')[1]
-      const [user, password] = Buffer.from(auth, 'base64').toString().split(':')
+      const [user, password] = atob(auth).split(':')
 
       if (user === process.env.AUTH_USER && password === process.env.AUTH_PASSWORD) {
         return NextResponse.next()
       }
     }
 
-    return new NextResponse('Auth required', {
+    return new NextResponse('Authentication required', {
       status: 401,
       headers: {
         'WWW-Authenticate': 'Basic realm="Secure Area"',
